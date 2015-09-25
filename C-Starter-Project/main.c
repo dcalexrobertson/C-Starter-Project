a1389b0911c6c29b3469e5ac3ca5c8e733e22b00
Alex Robertson <hello_im_alex@hotmail.com>
2015-09-25 16:06:43 -0700

    FizzBuzz Assignment

--- C-Starter-Project/main.c
+++ C-Starter-Project/main.c
@@ -9,7 +9,21 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    //FizzBuzz Assignment by Alex Robertson
+    
+    for (int i = 1; i <= 100; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            printf("FizzBuzz\n");
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else if (i % 3 == 0) {
+            printf("Fizz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+    
     printf("Hello, World!\n");
     return 0;
 }
