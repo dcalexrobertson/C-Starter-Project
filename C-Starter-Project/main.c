8459435b1a0503800e6b2eda1dd09df2ad800d01
Alex Robertson <hello_im_alex@hotmail.com>
2015-09-26 20:28:40 -0700

    FuzzBizzNezz Assignment

--- C-Starter-Project/main.c
+++ C-Starter-Project/main.c
@@ -1,11 +1,3 @@
-//
-//  main.c
-//  C-Starter-Project
-//
-//  Created by Daniel Mathews on 2015-02-02.
-//  Copyright (c) 2015 com.lighthouse-labs. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
@@ -23,7 +15,37 @@ int main(int argc, const char * argv[]) {
             printf("%d\n", i);
         }
     }
+
+    //FuzzBizzNezz Assignment by Alex Robertson    
     
-    printf("Hello, World!\n");
+    int fuzz;
+    int bizz;
+    int nezz;
+    
+    printf("Please enter a number: ");
+    scanf("%d", &fuzz);
+    
+    printf("Please enter a second number: ");
+    scanf("%d", &bizz);
+    
+    printf("Please enter a third number: ");
+    scanf("%d", &nezz);
+    
+    for (int i = 1; i <= nezz; i++) {
+        if (i % bizz == 0 && i % fuzz == 0) {
+            printf("FuzzBizz\n");
+        } else if (i % bizz == 0) {
+            printf("Bizz\n");
+        } else if (i % fuzz == 0) {
+            printf("Fuzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+    
     return 0;
 }
+
+
+    
+
